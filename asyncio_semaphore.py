dcddb8bff92ed1b804c1db258fd1be6e4f1c9fe8
Roman Beskokotov <roma@gmail.com>
2025-10-15 15:43:19 +0300

    Тест Semaphore

--- asyncio_semaphore.py
+++ asyncio_semaphore.py
@@ -2,6 +2,24 @@
 # Сервер может ограничивать кол-во запросов, которые он может обработать
 # Чтобы это исправить можно, например, написать полностью синхронный код, но все будет работать медленнее
 # Или можно использовать Semaphore. Он позволяет ограничить кол-во параллельно выполняющ. коррутин
-# aquire - заняли коррутинную функцию
+# acquire - заняли коррутинную функцию
 # release - освободили коррутинную функцию
 # То есть с помощью этих ключевых слов мы ограничиваем кол-во параллельно выполняющ. коррутин
+
+import asyncio
+from asyncio import Semaphore
+num = 0
+
+async def api_test(semaphore: Semaphore):
+    async with semaphore:
+        global num
+        num += 1
+        print(f'working {num}')
+        await asyncio.sleep(1)
+
+async def main():
+    semaphore = Semaphore(3)
+    await asyncio.gather(*[api_test(semaphore) for _ in range(50)])
+
+if __name__ == '__main__':
+    asyncio.run(main())
